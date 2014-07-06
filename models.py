6c4c63eacbded45a98b4f2f5a28b3e3a1c49ced9
Brian Williams <bgawilliams@gmail.com>
2014-07-05 22:46:28 -0400

    Comments
    comments is fun

--- models.py
+++ models.py
@@ -6,11 +6,13 @@ class Tempy(models.Model):
 	is_test = models.BooleanField()
 
 class Media(models.Model):
-        shelf = models.ForeignKey(Shelf)
+        shelf = models.ForeignKey(Shelf) #or maybe subshelf?
         name = models.CharField(max_length=100)
         UPC = models.DecimalField(max_digits=12, decimal_places=None)
         checked_out = models.BooleanField(default=False)
         who_has = models.CharField(max_length=100, default = 'Brian')
+        #year?  looks complicated to do "right"
+        #genre?
         #something to keep a series together?  looks like ManyToManyField?
         #link to wikipedia?
 
@@ -18,11 +20,14 @@ class DVD(Media):
         ripped_to_server = models.BooleanField(default=False)
         tv_show = models.BooleanField(default=False)
         box_set = models.BooleanField(default=False)
+        director = models.CharField(max_length=100)
+        #actors = many to many?
         #link to imdb?
 
 class Book(Media):
         author = models.CharField(max_length=100)
         paperback = models.BooleanField(default=False)
+        own_ebook = models.BooleanField(default=False)
         #link to amazon?
 
 class Location(models.Model): #entertainment, piano, tall bookcase
