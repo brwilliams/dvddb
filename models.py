0f07059f05549bd9a5ed478aab217a86a9f70ec7
Brian Williams <bgawilliams@gmail.com>
2014-07-06 00:56:30 -0400

    More notes

--- models.py
+++ models.py
@@ -6,7 +6,8 @@ class Media(models.Model):
         UPC = models.DecimalField(max_digits=12, decimal_places=None)
         checked_out = models.BooleanField(default=False)
         who_has = models.CharField(max_length=100, default = 'Brian')
-        copies = models.IntegerField(default=1)
+        copies = models.PositiveSmallIntegerField(default = 1)
+        condition = models.CharField(max_length=1000)
         #year?  looks complicated to do "right"
         #genre?
         #something to keep a series together?  looks like ManyToManyField?
@@ -19,7 +20,11 @@ class DVD(Media):
         ripped_to_server = models.BooleanField(default=False)
         tv_show = models.BooleanField(default=False)
         box_set = models.BooleanField(default=False)
+        widescreen = models.BooleanField(default=True)
+        blu_ray = models.BooleanField(default=False)
+        number_of_discs = models.PositiveSmallIntegerField(default = 1)
         director = models.CharField(max_length=100) #maybe make a director class
+        runtime_in_minutes = models.PositiveSmallIntegerField() #is there a better way to do this
         #actors = many to many?
         #link to imdb?
 
@@ -27,10 +32,12 @@ class Book(Media):
         author = models.CharField(max_length=100)
         paperback = models.BooleanField(default=False)
         own_ebook = models.BooleanField(default=False)
+        pages = models.PositiveSmallIntegerField()
+        #something like "if textbook, what course?"
 
 class CD(Media): #don't think I'll do this, but just in case
         artist = models.CharField(max_length=100)
-        tracks = models.PositiveSmallIntegerField()
+        number_of_tracks = models.PositiveSmallIntegerField()
 
 class VideoGames(Media): #same
         console = models.ForeignKey(Console)
