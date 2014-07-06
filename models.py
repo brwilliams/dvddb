ab9e4ae33a5c799cfb7b88b248a2b88644b701d6
Brian Williams <bgawilliams@gmail.com>
2014-07-05 23:00:47 -0400

    __unicorn__

--- models.py
+++ models.py
@@ -1,17 +1,18 @@
 from django.db import models
 
-# Create your models here.
-
 class Media(models.Model):
         shelf = models.ForeignKey(Shelf) #or maybe subshelf?
         name = models.CharField(max_length=100)
         UPC = models.DecimalField(max_digits=12, decimal_places=None)
         checked_out = models.BooleanField(default=False)
         who_has = models.CharField(max_length=100, default = 'Brian')
+        copies = models.IntegerField(default=1)
         #year?  looks complicated to do "right"
         #genre?
         #something to keep a series together?  looks like ManyToManyField?
         #link to wikipedia?
+        def __unicode__(self):
+		return self.name
 
 class DVD(Media):
         ripped_to_server = models.BooleanField(default=False)
@@ -30,13 +31,19 @@ class Book(Media):
 class Location(models.Model): #entertainment, piano, tall bookcase
         name = models.CharField(max_length=100)
         room = models.CharField(max_length=100)
+        def __unicode__(self):
+		return self.name
         
 class Shelf(models.Model): #first shelf, cabinet, etc
         location = models.ForeignKey(Location)
         name = models.CharField(max_length=100)
+        def __unicode__(self):
+		return self.name
 
 class SubShelf(models.Model): #avengers, classics
         #import from shelf again?
         shelf = models.ForeignKey(Shelf)
         name = models.CharField(max_length=100)
         #series?
+        def __unicode__(self):
+		return self.name
