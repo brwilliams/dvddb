5fffe62b956b55feeda5f7b3c4f5fe0a7c2185c7
Brian Williams <brwilliams@users.noreply.github.com>
2014-07-05 22:58:54 -0400

    Update views.py

--- views.py
+++ views.py
@@ -3,5 +3,6 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
 from django.views import generic
 from django.utils import timezone
-#from dvd.models import LIST MODELS I GUESS
+#from dvd.models import LIST MODELS I GUESS.  maybe I can just do import *?
+from decimal import * #don't remember if I need this
 # Create your views here.
