93a6706f6c9904a6fcd9a31b4be02b2feb90d376
Brian Williams <brwilliams@users.noreply.github.com>
2014-07-05 22:51:13 -0400

    Views imports

--- views.py
+++ views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect, HttpResponse
+from django.core.urlresolvers import reverse
+from django.views import generic
+from django.utils import timezone
+#from dvd.models import LIST MODELS I GUESS
 # Create your views here.
