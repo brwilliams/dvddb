7965e77b842cda8fb1f240c0c91d37f733248690
Brian Williams <brwilliams@users.noreply.github.com>
2014-07-05 23:00:32 -0400

    Update admin.py

--- admin.py
+++ admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from dvd.models import Tempy
+from dvd.models import * #hope this works
 
-admin.site.register(Tempy)
-# Register your models here.
+admin.site.register(DVD)
+admin.site.register(Book)
+admin.site.register(Location)
+admin.site.register(Shelf)
+admin.site.register(SubShelf)
