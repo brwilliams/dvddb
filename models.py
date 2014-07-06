a8362747bdd26e41670cc2aedb6fe782da04e416
Brian Williams <bgawilliams@gmail.com>
2014-07-05 22:46:46 -0400

    BYE TEMPY

--- models.py
+++ models.py
@@ -1,9 +1,6 @@
 from django.db import models
 
 # Create your models here.
-class Tempy(models.Model):
-	name = models.CharField(max_length=75)
-	is_test = models.BooleanField()
 
 class Media(models.Model):
         shelf = models.ForeignKey(Shelf) #or maybe subshelf?
